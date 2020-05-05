2883ccd9ba6a53faa9a731eafe3afe7678a68140
Olga <yagozaya@mail.ru>
2020-05-05 16:04:10 +0500

    Вывод на экран тип элемента списка.

---
+++ task 2_1.py
@@ -0,0 +1,3 @@
+my_list = ['text', 123, True, [1, 2, 3], None, {'a', 'b', 'c'}, {'key_1': 100, 'key_2': 200}]
+for i in range(len(my_list)):
+    print(f"Элемент {my_list[i]} соответствует типу {type(my_list[i])}")
