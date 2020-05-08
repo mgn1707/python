95c7f9d3ad85fdc9d589528fae06d4b7041da21b
Olga <yagozaya@mail.ru>
2020-05-08 16:57:05 +0500

    Вызов функции с несколькими параметрами и вывод их одной строкой.

---
+++ task 3_2.py
@@ -0,0 +1,4 @@
+def my_func(name, surname, year, city, email, phone):
+    print(name, surname, year, city, email, phone)
+
+my_func(name= 'Olga', surname='Ivanova', year=1994, city='Chelyabinsk', email='aaa@mail.ru', phone='12345678')
