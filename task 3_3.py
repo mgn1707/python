8a5cf37050826b853a8746ad70fa142eebc2bc2a
Olga <yagozaya@mail.ru>
2020-05-08 16:57:13 +0500

    Вывод суммы наибольших двух чисел.

---
+++ task 3_3.py
@@ -0,0 +1,4 @@
+def my_func(par_1, par_2, par_3):
+    excess = min(par_1, par_2, par_3)
+    return par_1 + par_2 + par_3 - excess
+print(my_func(int(input("Enter 1-st number - ")), par_2 = int(input("Enter 2-d number - ")), par_3 = int(input("Enter 3-d number - "))))
